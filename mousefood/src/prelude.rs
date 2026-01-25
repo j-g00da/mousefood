1ecb82293d1ec7f5422561a07416db8257ec0380
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2026-01-25 03:09:14 +0300

    docs(examples): improve examples in the documentation (#145)
    * docs(examples): improve the example snippets in documentation
    
    * chore(ci): run the doctests in CI
    
    * chore(markdownlint): allow details and summary tags
    
    * chore(lints): remove unnecessary main
    
    * chore(ci): test with all features

--- mousefood/src/prelude.rs
+++ mousefood/src/prelude.rs
@@ -2,8 +2,12 @@
 //!
 //! # Examples
 //!
-//! ```rust,no_run
+//! ```rust
+//! use mousefood::embedded_graphics::{mock_display::MockDisplay, pixelcolor::Rgb888};
 //! use mousefood::prelude::*;
+//!
+//! let mut display = MockDisplay::<Rgb888>::new();
+//! let _backend = EmbeddedBackend::new(&mut display, EmbeddedBackendConfig::default());
 //! ```
 
 pub use crate::backend::{EmbeddedBackend, EmbeddedBackendConfig};
